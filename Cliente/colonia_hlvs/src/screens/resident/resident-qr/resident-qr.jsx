f53413face76b2cdfb25f8eb48aa603522c8c1c3
AngelGuidos <00137321@uca.edu.sv>
2024-05-26 01:05:04 -0600

    Fix: Button now as component

--- Cliente/colonia_hlvs/src/screens/resident/resident-qr/resident-qr.jsx
+++ Cliente/colonia_hlvs/src/screens/resident/resident-qr/resident-qr.jsx
@@ -1,5 +1,7 @@
+import IconButton from '../../../components/buttons/IconButton/IconButton';
 import './resident-qr.css'
 import ErrorOutlineRoundedIcon from '@mui/icons-material/ErrorOutlineRounded';
+import QrCode2RoundedIcon from '@mui/icons-material/QrCode2Rounded';
 
 function ResidentQr(){
     return (
@@ -10,6 +12,12 @@ function ResidentQr(){
                     <ErrorOutlineRoundedIcon className='icon'/>
                     Tu codigo-QR se ha generado correctamente, acercate al escaner para entrar.
                 </div>
+                <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1200px-QR_code_for_mobile_English_Wikipedia.svg.png' alt='QR' className='myQR'/>
+                {/* <Button className='slim_button'>
+                    <QrCode2RoundedIcon className='button_icon'/>
+                    Refrescar
+                </Button> */}
+                <IconButton icon={<QrCode2RoundedIcon/>} text='Refrescar'/>
              </div>
              <div className='Right'>
 
