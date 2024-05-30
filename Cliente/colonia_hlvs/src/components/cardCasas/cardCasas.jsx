8878d229f2b2ef6c0abe6f5e4a0b2e4cb9cd5a3c
Mauriaguilar7 <00195721@uca.edu.sv>
2024-05-30 17:01:38 -0600

    Add: Estilos a botones de agregar y eliminar :art:

--- Cliente/colonia_hlvs/src/components/cardCasas/cardCasas.jsx
+++ Cliente/colonia_hlvs/src/components/cardCasas/cardCasas.jsx
@@ -1,14 +1,11 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { TextField } from '@mui/material';
 import IconButton from '../buttons/IconButton/IconButton';
 import './cardCasas.css';
-import { CheckBox } from '@mui/icons-material';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import { useState } from 'react';
@@ -19,49 +16,65 @@ import { toast } from 'react-toastify';
 
 
 export function BasicCard({ setRows, rows }) {
+
   const [houseNumber, setHouseNumber] = useState('');
   const [houseCapacity, setHouseCapacity] = useState('');
+  const [isUpdated, setIsUpdated] = useState(false);
+  const buttonClass = isUpdated ? 'button-update-enabled' : 'button-update-disabled';
+
 
   const handleHouseNumberChange = (event) => {
     setHouseNumber(event.target.value);
+    checkIfUpdated(event.target.value, houseCapacity);
   };
 
   const handleHouseCapacityChange = (event) => {
     setHouseCapacity(event.target.value);
+    checkIfUpdated(houseNumber, event.target.value);
+  };
+
+  const checkIfUpdated = (houseNumber, houseCapacity) => {
+    if (houseNumber && houseCapacity) {
+      setIsUpdated(true);
+    } else {
+      setIsUpdated(false);
+    }
   };
 
   const handleAdd = () => {
-    
-  const existingRowIndex = rows.findIndex(row => row.id === parseInt(houseNumber));
 
-  if (existingRowIndex !== -1) {
-    // Si la casa ya existe pero los campos están vacíos, actualízalos
-    const existingRow = rows[existingRowIndex];
-    if (existingRow.Casa === '-' || existingRow.Capacidad === '-') {
-      const newRows = [...rows];
-      newRows[existingRowIndex].Casa = `#${houseNumber}`;
-      newRows[existingRowIndex].Capacidad = houseCapacity;
-      setRows(newRows);
-      toast.success('La casa se creo correctamente');
+    const existingRowIndex = rows.findIndex(row => row.id === parseInt(houseNumber));
+
+    if (existingRowIndex !== -1) {
+      // Si la casa ya existe pero los campos están vacíos, se actualizanlos campos
+      const existingRow = rows[existingRowIndex];
+      if (existingRow.Casa === '-' || existingRow.Capacidad === '-') {
+        const newRows = [...rows];
+        newRows[existingRowIndex].Casa = `#${houseNumber}`;
+        newRows[existingRowIndex].Capacidad = houseCapacity;
+        setRows(newRows);
+        toast.success('La casa se creo correctamente');
+      } else {
+        toast.error('La casa ya existe');
+      }
     } else {
-      toast.error('La casa ya existe');
+      // Si la casa no existe, crea una nueva fila
+      const newRow = {
+        id: parseInt(houseNumber), // Usa el número de casa como ID
+        Contador: rows.length + 1,
+        Casa: `#${houseNumber}`,
+        Capacidad: houseCapacity,
+      };
+
+      // Agrega la nueva fila existentes
+      const newRows = [...rows, newRow];
+
+      setRows(newRows);
+
+      toast.success('La casa se agregó correctamente');
     }
-  } else {
-    // Si la casa no existe, crea una nueva fila
-    const newRow = {
-      id: parseInt(houseNumber), // Usa el número de casa como ID
-      Contador: rows.length + 1,
-      Casa: `#${houseNumber}`,
-      Capacidad: houseCapacity,
-    };
 
-    // Agrega la nueva fila existentes
-    const newRows = [...rows, newRow];
-
-    setRows(newRows);
-
-    toast.success('La casa se agregó correctamente');
-    }
+    setIsUpdated(false);
   };
 
 
@@ -93,6 +106,7 @@ export function BasicCard({ setRows, rows }) {
           />
 
           <IconButton
+            className={buttonClass}
             icon=''
             text='Agregar'
             disabled={!houseNumber || !houseCapacity}
@@ -109,16 +123,31 @@ export function SingleTextFieldCard({ rows, setRows }) {
 
   const [inputValue, setInputValue] = useState('');
   const [isChecked, setIsChecked] = useState(false);
+  const [isUpdated, setIsUpdated] = useState(false);
+  const buttonClass = isUpdated ? 'button-delete-enabled' : 'button-delete-disabled';
+
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
+    checkIfUpdated(event.target.value, isChecked);
   };
 
   const handleCheckboxChange = (event) => {
     setIsChecked(event.target.checked);
+    checkIfUpdated(inputValue, event.target.checked);
   };
 
+  const checkIfUpdated = (inputValue, isChecked) => {
+    if (inputValue && isChecked) {
+      setIsUpdated(true);
+    } else {
+      setIsUpdated(false);
+    }
+  };
+
+
   const handleDelete = () => {
+    // Busca la fila que contiene la casa con el número ingresado
     const rowIndex = rows.findIndex(row => row.Casa === `#${inputValue}`);
 
     if (rowIndex === -1) {
@@ -137,6 +166,8 @@ export function SingleTextFieldCard({ rows, setRows }) {
     setRows(newRows);
 
     toast.success('La casa se eliminó correctamente');
+    setIsUpdated(false);
+
   };
 
 
@@ -167,7 +198,13 @@ export function SingleTextFieldCard({ rows, setRows }) {
             sx={{ maxWidth: '75%', mt: 0, mb: 2 }}
           />
 
-          <IconButton icon='' text='Eliminar' disabled={!inputValue || !isChecked} onClick={handleDelete} />
+          <IconButton
+            className={buttonClass}
+            icon=''
+            text='Eliminar'
+            disabled={!inputValue || !isChecked}
+            onClick={handleDelete}
+          />
 
 
         </Box>
