137fa41b9e42cb0b1cb228810239187851dc836d
Mauriaguilar7 <00195721@uca.edu.sv>
2024-05-31 18:30:53 -0600

    Add: Corrigiedo Grafica

--- Cliente/colonia_hlvs/src/components/chart/chart.jsx
+++ Cliente/colonia_hlvs/src/components/chart/chart.jsx
@@ -2,23 +2,23 @@ import * as React from 'react';
 import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts';
 
 const data = [
-    { name: 'Lunes', uv: 4, color: '#FF5B5B' },
-    { name: 'Martes', uv: 3, color: '#F7C604' },
-    { name: 'Miercoles', uv: 5, color: '#FF5B5B' },
-    { name: 'Jueves', uv: 2, color: '#F7C604' },
-    { name: 'Viernes', uv: 1, color: '#FF5B5B' },
-    { name: 'Sabado', uv: 6, color: '#F7C604' },
-    { name: 'Domingo', uv: 3, color: '#FF5B5B' },
+    { name: 'Lunes', Ingresos: 4, color: '#FF5B5B' },
+    { name: 'Martes', Ingresos: 3, color: '#F7C604' },
+    { name: 'Miercoles', Ingresos: 5, color: '#FF5B5B' },
+    { name: 'Jueves', Ingresos: 2, color: '#F7C604' },
+    { name: 'Viernes', Ingresos: 1, color: '#FF5B5B' },
+    { name: 'Sabado', Ingresos: 6, color: '#F7C604' },
+    { name: 'Domingo', Ingresos: 3, color: '#FF5B5B' },
 ];
 
 export default function BasicBars() {
     return (
         <BarChart  width={900    } height={300} data={data} barSize={35}>
-            <CartesianGrid strokeDasharray="1 1" strokeWidth={0.7}  horizontal={true} vertical={false} />      
+            <CartesianGrid strokeDasharray="1 1" strokeWidth={0.7}  horizontal={true} vertical={false} /> 
             <XAxis dataKey="name" />
             <YAxis />
-            <Tooltip />
-            <Bar dataKey="uv" fill="#8884d8" shape={<RenderShape />} />
+            <Tooltip />     
+            <Bar dataKey="Ingresos" fill="#8884d8" shape={<RenderShape />} />
         </BarChart>
     );
 }
