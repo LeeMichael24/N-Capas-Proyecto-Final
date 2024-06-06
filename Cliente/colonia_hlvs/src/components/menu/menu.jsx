0e7ddb34f7345a8f79b5562fd8e953b3595e450c
LeeMichael24 <leemichaeln24@gmail.com>
2024-06-06 13:34:24 -0700

    Adding menu bottoms ux

--- Cliente/colonia_hlvs/src/components/menu/menu.jsx
+++ Cliente/colonia_hlvs/src/components/menu/menu.jsx
@@ -5,25 +5,24 @@ import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 const Menu = ({ buttons }) => {
-	/* funcion para medir el espacio y contener los botones, segun los que genere */
-	const [containerHeight, setContainerHeight] = useState('auto');
+  /* función para medir el espacio y contener los botones, según los que genere */
+  const [containerHeight, setContainerHeight] = useState('auto');
 
-	useEffect(() => {
-			const calculateHeight = () => {
-					const buttonHeight = 110; // Altura del botón en px
-					const buttonMargin = 20; // Margen superior e inferior del botón en px
-					const buttonsPerRow = 2; // Número de botones por fila
+  useEffect(() => {
+    const calculateHeight = () => {
+      const buttonHeight = 110; // Altura del botón en px
+      const buttonMargin = 20; // Margen superior e inferior del botón en px
+      const buttonsPerRow = 2; // Número de botones por fila
 
-					const numRows = Math.ceil(buttons.length / buttonsPerRow);
-					const totalHeight = numRows * (buttonHeight + buttonMargin);
-					setContainerHeight(`${totalHeight}px`);
-			};
+      const numRows = Math.ceil(buttons.length / buttonsPerRow);
+      const totalHeight = numRows * (buttonHeight + buttonMargin);
+      setContainerHeight(`${totalHeight}px`);
+    };
 
-			calculateHeight();
-	}, [buttons]);
+    calculateHeight();
+  }, [buttons]);
 
   const isSingleButton = buttons.length === 1; // Verificar si hay solo un botón
-
 
   const location = useLocation();
   const navigate = useNavigate();
@@ -32,17 +31,16 @@ const Menu = ({ buttons }) => {
     navigate(path);
   };
 
-
   return (
-    <div className="menu-container" style={{ height: containerHeight}}>
+    <div className="menu-container" style={{ height: containerHeight }}>
       {buttons.map((button, index) => (
-        <ButtonMenu 
+        <ButtonMenu
           key={index}
           icon={button.icon}
           name={button.name}
           path={button.path}
           isSingleButton={isSingleButton}
-          className={`menu-button ${location.pathname === button.path ? 'selected' : ''}`}
+          isSelected={location.pathname === button.path} // Pasa la prop isSelected
           onClick={() => handleButtonClick(button.path)}
         />
       ))}
